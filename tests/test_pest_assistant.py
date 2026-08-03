d70ef193f4d0c764fb913046ad2282d568f2be7c
Lavanya-Ar <2401617@sit.singaporetech.edu.sg>
2026-08-03 21:34:44 +0700

    chatbot app

--- tests/test_pest_assistant.py
+++
@@ -1,8 +0,0 @@
-from app.pest_assistant import load_class_names
-
-
-def test_load_class_names_returns_expected_labels():
-    labels = load_class_names()
-    assert labels[0] == "grub"
-    assert len(labels) == 10
-
